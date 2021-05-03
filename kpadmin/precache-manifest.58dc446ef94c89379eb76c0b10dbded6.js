b7280704b69dccd34a146c106b4391a53d9f57f5
天草降临 <radarzhhua@gmail.com>
2021-05-03 15:35:20 +0800

    增加开盘交易合约

---
+++ kpadmin/precache-manifest.58dc446ef94c89379eb76c0b10dbded6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2460b793b91633bdcd1cf5aaa164d6d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af90d8dfc8a53aed4ed3",
+    "url": "./static/css/2.af774a2d.chunk.css"
+  },
+  {
+    "revision": "ffd547ae081b5c2aa237",
+    "url": "./static/css/main.8921ce06.chunk.css"
+  },
+  {
+    "revision": "af90d8dfc8a53aed4ed3",
+    "url": "./static/js/2.09ff8d43.chunk.js"
+  },
+  {
+    "revision": "ffd547ae081b5c2aa237",
+    "url": "./static/js/main.b742bf64.chunk.js"
+  },
+  {
+    "revision": "c3775b24d7869a22637f",
+    "url": "./static/js/runtime~main.3451b984.js"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "./static/media/circle.2d975615.svg"
+  },
+  {
+    "revision": "c5fd67a763f34abbd647b3751bf49f47",
+    "url": "./static/media/favicon.c5fd67a7.ico"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "./static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "./static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "./static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "./static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
